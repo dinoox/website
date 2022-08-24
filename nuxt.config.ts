91b2760fd7997c3930670c6116c74b25e2e756e2
wangxiaoyu <dinoox@163.com>
2022-08-24 15:52:07 +0800

    edit base config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,20 @@ import vitePluginString from 'vite-plugin-string';
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+    app: {
+        head: {
+            meta: [
+                { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+            ],
+            link: [],
+            style: [],
+            script: [],
+            noscript: [
+                { children: 'Javascript is required' }
+            ]
+        }
+    },
+
     css: [
         '@/assets/theme/base.scss',
         '@/node_modules/animate.css/animate.css',

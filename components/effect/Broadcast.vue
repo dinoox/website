06e6df83e1f2f574b808115fc798a048abeba3b8
wangxiaoyu <dinoox@163.com>
2022-08-26 11:21:09 +0800

    finish banner

--- components/effect/Broadcast.vue
+++ components/effect/Broadcast.vue
@@ -1,55 +1,60 @@
 <template>
-  <header class="marquee-header node-marquee" translate="no"
-    style="width: 100%; overflow: hidden; white-space: nowrap;">
-    <div class="float-font1"
-      style="display: inline-block; transform: matrix3d(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 392, 0, 0, 1);">
-      &nbsp;Awards
-      — Recognition —
-    </div>
-    <div class="float-font"
-      style="position: absolute; top: 0; left: 0; display: inline-block; transform: matrix3d(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1336, 0, 0, 1);">
+  <section class="marquee-header w-100 text-truncate">
+    <div class="banner d-inline-block">
       &nbsp;Awards — Recognition —
     </div>
-    <div class="float-font"
-      style="position: absolute; top: 0; left: 0; display: inline-block; transform: matrix3d(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 2280, 0, 0, 1);">
+    <div v-for="n in 3" class="banner position-absolute d-inline-block" style="top: 0; left: 0;">
       &nbsp;Awards — Recognition —
     </div>
-    <div class="float-font"
-      style="position: absolute; top: 0; left: 0; display: inline-block; transform: matrix3d(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, -551, 0, 0, 1);">
-      &nbsp;Awards — Recognition —
-    </div>
-  </header>
+  </section>
 </template>
 
 <script setup>
 import { onMounted } from 'vue'
 
-const { $decomposeMatrix3D } = useNuxtApp()
-
-
-let arr1 = [
+let arr = [
   1, 0, 0, 0,
   0, 1, 0, 0,
   0, 0, 1, 0,
-  392, 0, 0, 1
-];
+  0, 0, 0, 1
+]
 
-function changeX(node, width) {
-  console.log("width", width);
-  if (arr1[12] <= -width) arr1[12] = 4 * width
-  console.log("xx");
-  arr1[12] -= 5
-  node.style.transform = "matrix3d(" + arr1.join(',') + ")"
-  requestAnimationFrame(() => changeX(node, width))
+let arrs = [arr, [...arr], [...arr], [...arr]]
+let bannerWidth, speed;
+let nodes = []
+
+
+function init() {
+  speed = 3;
+  bannerWidth = document.querySelector('.banner').offsetWidth
+  arrs[0][12] = 0
+  arrs[1][12] = bannerWidth
+  arrs[2][12] = bannerWidth * 2
+  arrs[3][12] = bannerWidth * 3
+
+  for (let i = 1; i <= 4; i++) {
+    let node = document.querySelector(`.banner:nth-child(${i})`)
+    nodes.push(node)
+    node.style.transform = getMatrix3D(arrs[i - 1])
+  }
 }
+
+function getMatrix3D(arr) {
+  return `matrix3d(${arr.join(',')})`
+}
+
+function float() {
+  for (let i = 1; i <= 4; i++) {
+    let arr = arrs[i - 1];
+    arr[12] <= -bannerWidth ? arr[12] = 3 * bannerWidth : arr[12] -= speed
+    nodes[i - 1].style.transform = getMatrix3D(arr)
+  }
+  requestAnimationFrame(float)
+}
+
 onMounted(() => {
-  const width = document.querySelector('.float-font1').offsetWidth
-  let node = document.querySelector('.float-font1')
-
-  node.style.transform = "matrix3d(" + arr1.join(',') + ")"
-
-
-  requestAnimationFrame(() => changeX(node, width))
+  init()
+  float()
 })
 </script>
 
